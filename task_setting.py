650bcb8aad78fbf559b15183e9602337219683f9
Kailejiang <93170369+Kailejiang@users.noreply.github.com>
2025-04-11 20:30:07 +0800

    Add files via upload

---
+++ task_setting.py
@@ -0,0 +1,71 @@
+import pytorch_lightning as pl
+import torch
+import torchmetrics
+
+
+class Task(pl.LightningModule):
+    def __init__(
+        self,
+        name="energy_U0",
+        model=None,
+        loss_fn=torch.nn.MSELoss(),
+        loss_weight=1.0,
+        metrics={"MAE": torchmetrics.MeanAbsoluteError()},
+        optimizer_cls=torch.optim.AdamW,
+        optimizer_args={"lr": 1e-5},
+    ):
+        super().__init__()
+        self.name = name
+        self.model = model
+        self.loss_fn = loss_fn
+        self.loss_weight = loss_weight
+        self.metrics = torch.nn.ModuleDict(metrics)
+        self.optimizer_cls = optimizer_cls
+        self.optimizer_args = optimizer_args
+
+        # 保存超参数
+        self.save_hyperparameters(ignore=["model"])
+
+    def forward(self, batch):
+        return self.model(batch)
+
+    def _compute_loss_and_metrics(self, batch, batch_idx, stage):
+        # 从 batch 中提取目标值
+        targets = {self.name: batch[self.name]}
+        pred = self(batch)  # 前向传播，生成预测值
+
+        # 计算损失
+        loss = self.loss_weight * self.loss_fn(pred[self.name], targets[self.name])
+
+        # 记录损失
+        self.log(f"{stage}_loss", loss, on_step=(stage == "train"), on_epoch=(stage != "train"), prog_bar=True)
+
+        # 将验证损失传递给 Atomwise 模型以更新 best_val_loss
+        if stage == "val":
+            self.model.forward(batch, val_loss=loss.item())
+
+        # 计算并记录评估指标
+        for name, metric in self.metrics.items():
+            metric_value = metric(pred[self.name], targets[self.name])
+            self.log(f"{stage}_{name}", metric_value, on_step=(stage == "train"), on_epoch=(stage != "train"), prog_bar=False)
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        return self._compute_loss_and_metrics(batch, batch_idx, "train")
+
+    def validation_step(self, batch, batch_idx):
+        return self._compute_loss_and_metrics(batch, batch_idx, "val")
+
+    def test_step(self, batch, batch_idx):
+        return self._compute_loss_and_metrics(batch, batch_idx, "test")
+
+    def configure_optimizers(self):
+        return self.optimizer_cls(self.parameters(), **self.optimizer_args)
+
+    def on_train_epoch_end(self):
+        self.model.all_molecular_features.clear()
+
+    def save_model(self, path: str):
+        if self.global_rank == 0:
+            torch.save(self.model, path)
