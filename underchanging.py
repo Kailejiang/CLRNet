fa5d58d2ecbd07a2cde0b08a4abc9c71d6a826ad
Kailejiang <93170369+Kailejiang@users.noreply.github.com>
2025-05-30 12:00:41 +0800

    Create underchanging.py

---
+++ underchanging.py
@@ -0,0 +1,60 @@
+from ase.md import Langevin, MDLogger
+from ase.io.trajectory import Trajectory
+
+    def init_md(
+        self,
+        name: str,
+        time_step: float = 0.5,
+        temp_init: float = 300,
+        temp_bath: Optional[float] = 300,
+        reset: bool = False,
+        interval: int = 1,
+    ):
+        """
+        Initialize an ase molecular dynamics trajectory. The logfile needs to
+        be specifies, so that old trajectories are not overwritten. This
+        functionality can be used to subsequently carry out equilibration and
+        production.
+
+        Args:
+            name: Basic name of logfile and trajectory
+            time_step: Time step in fs (default=0.5)
+            temp_init: Initial temperature of the system in K (default is 300)
+            temp_bath: Carry out Langevin NVT dynamics at the specified
+                temperature. If set to None, NVE dynamics are performed
+                instead (default=None)300
+            reset: Whether dynamics should be restarted with new initial
+                conditions (default=False)
+            interval: Data is stored every interval steps (default=1)
+        """
+
+        # If a previous dynamics run has been performed, don't reinitialize
+        # velocities unless explicitly requested via restart=True
+        if self.dynamics is None or reset:
+            self._init_velocities(temp_init=temp_init)
+
+        # Set up dynamics
+        self.dynamics = Langevin(
+            self.molecule,
+            time_step * units.fs,
+            temp_bath * units.kB,
+            1.0 / (100.0 * units.fs),
+        )
+
+        # Create monitors for logfile and a trajectory file
+        logfile = os.path.join(self.working_dir, "{:s}.log".format(name))
+        trajfile = os.path.join(self.working_dir, "{:s}.traj".format(name))
+        logger = MDLogger(
+            self.dynamics,
+            self.molecule,
+            logfile,
+            stress=False,
+            peratom=False,
+            header=True,
+            mode="a",
+        )
+        trajectory = Trajectory(trajfile, "w", self.molecule)
+
+        # Attach monitors to trajectory
+        self.dynamics.attach(logger, interval=interval)
+        self.dynamics.attach(trajectory.write, interval=interval)
