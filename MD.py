de976dcf2382033a82d4f7da846863259fbe37bd
Kailejiang <93170369+Kailejiang@users.noreply.github.com>
2025-06-04 19:14:17 +0800

    Update MD.py

--- MD.py
+++ MD.py
@@ -33,12 +33,122 @@ print('Average temperature: {:10.2f} K'.format(np.mean(temperature)))
 
 plt.show()
 
+class AseInterface:
+    """
+    Interface for ASE calculations (optimization and molecular dynamics)
+    """
+
+    def __init__(
+        self,
+        molecule_path: str,
+        working_dir: str,
+        model_file: str,
+        neighbor_list: schnetpack.transform.Transform,
+        energy_key: str = "energy",
+        force_key: str = "forces",
+        stress_key: Optional[str] = None,
+        energy_unit: Union[str, float] = "kcal/mol",
+        position_unit: Union[str, float] = "Angstrom",
+        device: Union[str, torch.device] = "cpu",
+        dtype: torch.dtype = torch.float32,
+        converter: AtomsConverter = AtomsConverter,
+        optimizer_class: type = QuasiNewton,
+        fixed_atoms: Optional[List[int]] = None,
+        transforms: Union[
+            schnetpack.transform.Transform, List[schnetpack.transform.Transform]
+        ] = None,
+        additional_inputs: Dict[str, torch.Tensor] = None,
+    ):
+        """
+        Args:
+            molecule_path: Path to initial geometry
+            working_dir: Path to directory where files should be stored
+            model_file (str): path to trained model
+            neighbor_list (schnetpack.transform.Transform): SchNetPack neighbor list
+            energy_key (str): name of energies in model (default="energy")
+            force_key (str): name of forces in model (default="forces")
+            stress_key (str): name of stress tensor in model. Will not be computed if set to None (default=None)
+            energy_unit (str, float): energy units used by model (default="kcal/mol")
+            position_unit (str, float): position units used by model (default="Angstrom")
+            device (torch.device): device used for calculations (default="cpu")
+            dtype (torch.dtype): select model precision (default=float32)
+            converter (schnetpack.interfaces.AtomsConverter): converter used to set up input batches
+            optimizer_class (ase.optimize.optimizer): ASE optimizer used for structure relaxation.
+            fixed_atoms (list(int)): list of indices corresponding to atoms with positions fixed in space.
+            transforms (schnetpack.transform.Transform, list): transforms for the converter. More information
+                can be found in the AtomsConverter docstring.
+            additional_inputs (dict): additional inputs required for some transforms in the converter.
+        """
+        # Setup directory
+        self.working_dir = working_dir
+        if not os.path.exists(self.working_dir):
+            os.makedirs(self.working_dir)
+
+        # Load the molecule
+        self.molecule = read(molecule_path)
+
+        # Apply position constraints
+        if fixed_atoms:
+            c = FixAtoms(fixed_atoms)
+            self.molecule.set_constraint(constraint=c)
+
+        # Set up optimizer
+        self.optimizer_class = optimizer_class
+
+        # Set up calculator
+        calculator = SpkCalculator(
+            model_file=model_file,
+            neighbor_list=neighbor_list,
+            energy_key=energy_key,
+            force_key=force_key,
+            stress_key=stress_key,
+            energy_unit=energy_unit,
+            position_unit=position_unit,
+            device=device,
+            dtype=dtype,
+            converter=converter,
+            transforms=transforms,
+            additional_inputs=additional_inputs,
+        )
+
+        self.molecule.set_calculator(calculator)
+
+        self.dynamics = None
+
+    def save_molecule(self, name: str, file_format: str = "xyz", append: bool = False):
+        """
+        Save the current molecular geometry.
+
+        Args:
+            name: Name of save-file.
+            file_format: Format to store geometry (default xyz).
+            append: If set to true, geometry is added to end of file (default False).
+        """
+        molecule_path = os.path.join(
+            self.working_dir, "{:s}.{:s}".format(name, file_format)
+        )
+        write(molecule_path, self.molecule, format=file_format, append=append)
+
+    def calculate_single_point(self):
+        """
+        Perform a single point computation of the energies and forces and
+        store them to the working directory. The format used is the extended
+        xyz format. This functionality is mainly intended to be used for
+        interfaces.
+        """
+        energy = self.molecule.get_potential_energy()
+        forces = self.molecule.get_forces()
+        self.molecule.energy = energy
+        self.molecule.forces = forces
+
+        self.save_molecule("single_point", file_format="xyz")
+
     def init_md(
         self,
         name: str,
         time_step: float = 0.5,
         temp_init: float = 300,
-        temp_bath: Optional[float] = 300,
+        temp_bath: Optional[float] = None,
         reset: bool = False,
         interval: int = 1,
     ):
@@ -54,7 +164,7 @@ plt.show()
             temp_init: Initial temperature of the system in K (default is 300)
             temp_bath: Carry out Langevin NVT dynamics at the specified
                 temperature. If set to None, NVE dynamics are performed
-                instead (default=None)300
+                instead (default=None)
             reset: Whether dynamics should be restarted with new initial
                 conditions (default=False)
             interval: Data is stored every interval steps (default=1)
@@ -66,12 +176,15 @@ plt.show()
             self._init_velocities(temp_init=temp_init)
 
         # Set up dynamics
-        self.dynamics = Langevin(
-            self.molecule,
-            time_step * units.fs,
-            temp_bath * units.kB,
-            1.0 / (100.0 * units.fs),
-        )
+        if temp_bath is None:
+            self.dynamics = VelocityVerlet(self.molecule, time_step * units.fs)
+        else:
+            self.dynamics = Langevin(
+                self.molecule,
+                time_step * units.fs,
+                temp_bath * units.kB,
+                1.0 / (100.0 * units.fs),
+            )
 
         # Create monitors for logfile and a trajectory file
         logfile = os.path.join(self.working_dir, "{:s}.log".format(name))
@@ -90,3 +203,23 @@ plt.show()
         # Attach monitors to trajectory
         self.dynamics.attach(logger, interval=interval)
         self.dynamics.attach(trajectory.write, interval=interval)
+
+    def _init_velocities(
+        self,
+        temp_init: float = 300,
+        remove_translation: bool = True,
+        remove_rotation: bool = True,
+    ):
+        """
+        Initialize velocities for molecular dynamics
+
+        Args:
+            temp_init: Initial temperature in Kelvin (default 300)
+            remove_translation: Remove translation components of velocity (default True)
+            remove_rotation: Remove rotation components of velocity (default True)
+        """
+        MaxwellBoltzmannDistribution(self.molecule, temp_init * units.kB)
+        if remove_translation:
+            Stationary(self.molecule)
+        if remove_rotation:
+            ZeroRotation(self.molecule)
